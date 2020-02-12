47d287efd8e294d4441d03123d4e033e04ac29cf
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-12 14:24:30 -0500

    first commit -- running the server -- commented out sections dedicated to reqs

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+//imports
 const express = require('express');
-
 const server = express();
 
+//middleware stack
+server.use(express.json())
+
+//custom middleware!
+function logger(req, res, next) {}
+
+//routes
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+//server listen 5000
+server.listen(5000, () => {
+  console.log(`\n Peer into Port 5000 for your future \n`)
+})
 
-function logger(req, res, next) {}
-
+//export
 module.exports = server;
