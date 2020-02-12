9bf98d49a79b49a773e2a91594bf3182cd2c524b
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-12 14:31:24 -0500

    server was acting funny, fixed some typos

--- server.js
+++ server.js
@@ -1,22 +1,31 @@
-//imports
+//-----imports-----
 const express = require('express');
 const server = express();
 
-//middleware stack
+
+//-----middleware stack-----
 server.use(express.json())
 
-//custom middleware!
-function logger(req, res, next) {}
 
-//routes
-server.get('/', (req, res) => {
+//-----custom middleware!-----
+//logger middleware
+function logger(req, res, next) {
+  console.log(`LOGGER: ${req.method} request to ${req.originalUrl}`)
+  next()
+}
+
+
+//-----routes-----
+server.get('/', logger, (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//server listen 5000
+
+//-----server listen 5000-----
 server.listen(5000, () => {
   console.log(`\n Peer into Port 5000 for your future \n`)
 })
 
-//export
+
+//-----export-----
 module.exports = server;
