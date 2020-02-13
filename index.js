d8a5c25e26a6fadc9508443f3692e6978cabb1a0
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-12 20:47:22 -0500

    moved my .listen into index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+const server = require('./server.js');
+
+//-----server listen 5000-----
+server.listen(5000, () => {
+  console.log(`\n Peer into Port 5000 for your future \n`)
+})
--- server.js
+++ server.js
@@ -29,11 +29,5 @@ server.get('/', (req, res) => {
 });
 
 
-//-----server listen 5000-----
-server.listen(5000, () => {
-  console.log(`\n Peer into Port 5000 for your future \n`)
-})
-
-
 //-----export-----
 module.exports = server;
