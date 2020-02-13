ba5b933ad7465ba6642db16c54fd17d21f60656f
Zachary Mayfield <zakmayfield@gmail.com>
2020-02-13 10:07:35 -0500

    added .env file and set port and host to environment variables

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
+
 const server = require('./server.js');
 
+const port = process.env.PORT;
+const host = process.env.HOST;
+
 //-----server listen 5000-----
-server.listen(5000, () => {
-  console.log(`\n Peer into Port 5000 for your future \n`)
+server.listen(port, () => {
+  console.log(`\n Peer into ${host}${port} for your future \n`)
 })
